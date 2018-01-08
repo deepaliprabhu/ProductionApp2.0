88ac24c33b21a8784d43c12935ac31850a3d61cb
Andrei Ghidoarca <andreighidoarca@andreis-imac.local>
2018-01-08 13:50:13 +0200

    add message popup moved

--- DashboardApp/Source/Classes/Views/RunList/RunCommentsScreen.m
+++ DashboardApp/Source/Classes/Views/RunList/RunCommentsScreen.m
@@ -12,8 +12,10 @@
 #import "LoadingView.h"
 #import "ProdAPI.h"
 #import "AddCommentScreen.h"
+#import "Defines.h"
+#import "UserManager.h"
 
-@interface RunCommentsScreen () <AddCommentProtocol>
+@interface RunCommentsScreen () <UITextViewDelegate>
 
 @end
 
@@ -21,6 +23,8 @@
 {
     __weak IBOutlet UITableView *_tableView;
     __weak IBOutlet UILabel *_noCommentsLabel;
+    __weak IBOutlet UITextView *_messageTextView;
+    __weak IBOutlet UIView *_holderView;
     
     NSMutableArray *_comments;
 }
@@ -34,26 +38,27 @@
 
 #pragma mark - Actions
 
-- (IBAction) addCommentsButtonTapped {
- 
-    AddCommentScreen *screen = [[AddCommentScreen alloc] initWithNibName:@"AddCommentScreen" bundle:nil];
-    screen.run = _run;
-    screen.delegate = self;
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:screen];
-    nav.modalPresentationStyle = UIModalPresentationFormSheet;
-    [self presentViewController:nav animated:true completion:nil];
-}
-
 - (IBAction) cancelButtonTapped {
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
-#pragma mark - AddCommentProtocol
-
-- (void) newCommentAdded:(CommentModel *)comment {
+- (void) saveButtonTapped {
     
-    [_comments insertObject:comment atIndex:0];
-    [self layoutComments];
+    [self.view endEditing:true];
+    if (_messageTextView.text.length > 0)
+    {
+        [LoadingView showLoading:@"Loading..."];
+        NSString *runID = [NSString stringWithFormat:@"%d", (int)_run.runId];
+        [[ProdAPI sharedInstance] addComment:_messageTextView.text forRun:runID from:@"" withCompletion:^(BOOL success, id response) {
+            [self commentAddingFinishedWith:success];
+        }];
+    }
+    else
+        [LoadingView showShortMessage:@"Please insert a comment"];
+}
+
+- (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:true];
 }
 
 #pragma mark - UITableViewDelegate
@@ -93,6 +98,19 @@
     
     UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonTapped)];
     self.navigationItem.leftBarButtonItem = left;
+    
+    UIBarButtonItem *right = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveButtonTapped)];
+    self.navigationItem.rightBarButtonItem = right;
+    
+    [self addShadowTo:_holderView];
+}
+
+- (void) addShadowTo:(UIView*)v {
+    
+    v.layer.shadowOffset = ccs(0, 1);
+    v.layer.shadowColor = [UIColor blackColor].CGColor;
+    v.layer.shadowRadius = 2;
+    v.layer.shadowOpacity = 0.2;
 }
 
 - (void) layoutComments {
@@ -125,6 +143,24 @@
             [LoadingView showShortMessage:@"Error, please try again later!"];
         }
     }];
+}
+
+- (void) commentAddingFinishedWith:(BOOL)success {
+    
+    if (success)
+    {
+        _messageTextView.text = @"";
+        
+        [LoadingView removeLoading];
+        CommentModel *c = [CommentModel new];
+        c.date = [NSDate date];
+        c.author = [[UserManager sharedInstance] loggedUser].username;
+        c.message = _messageTextView.text;
+        [_comments insertObject:c atIndex:0];
+        [self layoutComments];
+    }
+    else
+        [LoadingView showShortMessage:@"Error, please try again later!"];
 }
 
 @end
