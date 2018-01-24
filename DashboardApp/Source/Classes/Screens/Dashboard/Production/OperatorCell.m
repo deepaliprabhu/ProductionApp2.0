232614058b130bc2e6e8d52282447840195acc88
Andrei Ghidoarca <andreighidoarca@andreis-imac.local>
2018-01-25 00:13:14 +0200

    operator schedule added

--- DashboardApp/Source/Classes/Screens/Dashboard/Production/OperatorCell.m
+++ DashboardApp/Source/Classes/Screens/Dashboard/Production/OperatorCell.m
@@ -7,13 +7,27 @@
 //
 
 #import "OperatorCell.h"
+#import "Defines.h"
 
 @implementation OperatorCell {
     __weak IBOutlet UILabel *_operatorLabel;
+    __weak IBOutlet UIView *_scheduleHolderView;
 }
 
 - (void) layoutWithPerson:(UserModel*)user {
+    
     _operatorLabel.text = user.name;
+    
+    for (int i=0; i<16; i++) {
+        
+        UIView *v = [_scheduleHolderView viewWithTag:i];
+        int s = (i+1)*30*60;
+        if (user.secondsBusy < s || (i==0 && s==0)) {
+            v.backgroundColor = ccolor(204, 204, 204);
+        } else {
+            v.backgroundColor = ccolor(51, 204, 51);
+        }
+    }
 }
 
 @end
--- DashboardApp/Source/Classes/Utils/Models/UserModel.h
+++ DashboardApp/Source/Classes/Utils/Models/UserModel.h
@@ -17,6 +17,7 @@
 @property (nonatomic, strong) NSString *role;
 @property (nonatomic, strong) NSString *status;
 @property (nonatomic, strong) NSString *points;
+@property (nonatomic, unsafe_unretained) int secondsBusy;
 
 + (UserModel*) objectFromData:(NSDictionary*)data;
 - (BOOL) isAdmin;
