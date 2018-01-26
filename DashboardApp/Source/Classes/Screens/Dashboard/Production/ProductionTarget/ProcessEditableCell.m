5946cc3d5f57ab539d4a0501461f3b04136a1628
Andrei Ghidoarca <andreighidoarca@andreis-imac.local>
2018-01-26 15:29:42 +0200

    production work

--- DashboardApp/Source/Classes/Screens/Dashboard/Production/ProductionTarget/ProcessEditableCell.m
+++ DashboardApp/Source/Classes/Screens/Dashboard/Production/ProductionTarget/ProcessEditableCell.m
@@ -18,7 +18,26 @@
 }
 
 - (void) layoutWithProcess:(ProcessModel*)process {
+    
     _processLabel.text = process.processName;
+    _timeLabel.text = [self timeForSeconds:[process.processingTime intValue]];
+}
+
+- (NSString*) timeForSeconds:(int)time {
+    
+    if (time == 0)
+        return @"-";
+    else {
+        
+        time = (time/60)*60;
+        int h = time/3600;
+        int min = (time%3600)/60;
+        if (h == 0) {
+            return [NSString stringWithFormat:@"%dm", min];
+        } else {
+            return [NSString stringWithFormat:@"%dh %dm", h, min];
+        }
+    }
 }
 
 @end
