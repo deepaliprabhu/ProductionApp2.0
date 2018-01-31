36f9dc6364da8d938cd04778c6607e464b91af1a
Andrei Ghidoarca <andreighidoarca@Andreis-iMac.local>
2018-01-31 22:16:41 +0200

    test branch
    test branch

--- DashboardApp/Source/Classes/Screens/Auth/SplashScreen.m
+++ DashboardApp/Source/Classes/Screens/Auth/SplashScreen.m
@@ -19,7 +19,7 @@
 @implementation SplashScreen
 
 - (void)viewDidLoad {
-    
+    //
     [super viewDidLoad];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
